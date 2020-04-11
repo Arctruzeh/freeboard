116d93719ceda56e07a701f13177b9a98dc0b4b6
A.J. Latocha <ajlato01@gmail.com>
2020-04-11 13:46:52 -0400

    load from current domain

--- plugins/midnetmedia/mmPlugins.js
+++ plugins/midnetmedia/mmPlugins.js
@@ -1,13 +1,14 @@
-$.getScript("plugins/midnetmedia/mmEngineControl.js")
-$.getScript("plugins/midnetmedia/mmInfoPanel.js")
-$.getScript("plugins/midnetmedia/mmValveControl.js")
-$.getScript("plugins/midnetmedia/mmWatchdog.js")
-$.getScript("plugins/midnetmedia/monitorGrid.js")
-$.getScript("plugins/midnetmedia/singleMonitor.js")
-$.getScript("plugins/midnetmedia/mmMomentaryPB.js")
+var mmDomain = window.location.href 
 
+$.getScript(mmDomain + "plugins/midnetmedia/mmEngineControl.js")
+$.getScript(mmDomain + "plugins/midnetmedia/mmInfoPanel.js")
+$.getScript(mmDomain + "plugins/midnetmedia/mmValveControl.js")
+$.getScript(mmDomain + "plugins/midnetmedia/mmWatchdog.js")
+$.getScript(mmDomain + "plugins/midnetmedia/monitorGrid.js")
+$.getScript(mmDomain + "plugins/midnetmedia/singleMonitor.js")
+$.getScript(mmDomain + "plugins/midnetmedia/mmMomentaryPB.js")
 $("<link/>", {
     rel: "stylesheet",
     type: "text/css",
-    href: "plugins/midnetmedia/mmPlugins.css"
+    href: mmDomain + "plugins/midnetmedia/mmPlugins.css"
  }).appendTo("head")
