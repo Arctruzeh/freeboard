7e07aad82188e708035c668c867de8880f0d7b62
A.J. Latocha <ajlato01@gmail.com>
2020-04-16 15:16:20 -0400

    disable everything besides engineControl

--- plugins/midnetmedia/mmPlugins.js
+++ plugins/midnetmedia/mmPlugins.js
@@ -6,14 +6,14 @@ if (document.domain === '127.0.0.1'){
     mmDomain = 'https://mmfreeboard.netlify.com/'
 }
 
-$.getScript(mmDomain + "plugins/midnetmedia/singleMonitor.js")
-$.getScript(mmDomain + "plugins/midnetmedia/monitorGrid.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmWatchdog.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmValveControl.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmCruiseControl.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmInfoPanel.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmMomentaryPB.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmSingleButton.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/singleMonitor.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/monitorGrid.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmWatchdog.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmValveControl.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmCruiseControl.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmInfoPanel.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmMomentaryPB.js")
+//$.getScript(mmDomain + "plugins/midnetmedia/mmSingleButton.js")
 $.getScript(mmDomain + "plugins/midnetmedia/mmEngineControl.js")
 
 $("<link/>", {
