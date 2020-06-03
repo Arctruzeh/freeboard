2c473184be9d541bdf5bd1cad9c7785246662b1d
A.J. Latocha <ajlato01@gmail.com>
2020-06-03 15:51:00 -0400

    >= goal throttle

--- plugins/midnetmedia/mmEngineControl.js
+++ plugins/midnetmedia/mmEngineControl.js
@@ -439,7 +439,7 @@
 
             if (settingName == "ec_goal_throttle") {
                 //console.log(newValue)
-                if (newValue > 0) {
+                if (newValue >= 0) {
                     document.getElementById(thisWidgetId).getElementsByClassName('ec-number')[0].innerHTML = newValue
                 } else {
                     document.getElementById(thisWidgetId).getElementsByClassName('ec-number')[0].innerHTML = "DC"
