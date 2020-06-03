f840f3a763dd6019cbf6c9a54324ac37700b7090
A.J. Latocha <ajlato01@gmail.com>
2020-06-03 16:53:06 -0400

    move momentaryPB styling to css file

--- plugins/midnetmedia/mmMomentaryPB.js
+++ plugins/midnetmedia/mmMomentaryPB.js
@@ -1,12 +1,5 @@
 (function () {
 
-    freeboard.addStyle('.pb-container', "width: 100%; height: 100%; background-image: radial-gradient(#48a7d9, #0c6494); display: flex; justify-content: center; align-items: center;")
-    freeboard.addStyle('.pb-button', "display: flex; justify-content: center; align-items: center; width: 50%; height: 75%; background: #555557; border: 1px solid #000000; box-shadow: 0 3px 6px 0 rgba(0,0,0,0.11); border-radius: 4px; margin-right: 8px;")
-    freeboard.addStyle('.pb-button-inner', 'display: flex; align-items: center; justify-content: center; width: 93%; height: 85%; background: #545456; border: 1px solid #909091; box-shadow: 0 0 6px 0 rgba(0,0,0,0.11), inset 0 0 3px 0 rgba(0,0,0,0.20); border-radius: 4px;')
-    freeboard.addStyle('.pb-button-bg', 'display: flex; justify-content: center; align-items: center; width: 97%; height: 90%; background-color: grey; border-radius: 4px 4px 4px 4px; margin-left: 1px; margin-top: 0.5px;')
-    freeboard.addStyle('.pb-button-label', 'display: flex; justify-content: center; align-items: center; overflow: hidden; width: 100%; height: 100%; background: linear-gradient(0deg, rgba(0,0,0,0.0) 0%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.0) 100%); text-shadow: 1px 1px 1px #000, -1px -1px 1px #000, 1px -1px 1px #000, -1px 1px 1px #000; color: white; border-radius: 4px 4px 4px 4px;')
-    freeboard.addStyle('.pb-button-label:active', 'background: linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.2) 50%, rgba(0,0,0,0) 100%);')
-
     //Definition
 
     freeboard.loadWidgetPlugin({
