9d1610b543964ed08fbe3d5dd8ae67a8fcbba5bf
A.J. Latocha <ajlato01@gmail.com>
2020-07-17 10:50:40 -0400

    Change mmPlugins to test loading issue

--- plugins/midnetmedia/mmPlugins.js
+++ plugins/midnetmedia/mmPlugins.js
@@ -1,24 +1,41 @@
-var mmDomain
+// var mmDomain
 
-if (document.domain === '127.0.0.1'){
-    mmDomain = ''
-} else {
-    mmDomain = 'https://mmfreeboard.netlify.app/'
-}
+// if (document.domain === '127.0.0.1'){
+//     mmDomain = ''
+// } else {
+//     mmDomain = 'https://mmfreeboard.netlify.app/'
+// }
 
-$.getScript(mmDomain + "plugins/midnetmedia/singleMonitor.js")
-$.getScript(mmDomain + "plugins/midnetmedia/monitorGrid.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmWatchdog.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmValveControl.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmCruiseControl.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmInfoPanel.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmMomentaryPB.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmSingleButton.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmEngineControl.js")
-$.getScript(mmDomain + "plugins/midnetmedia/mmTwoAction.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/singleMonitor.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/monitorGrid.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmWatchdog.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmValveControl.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmCruiseControl.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmInfoPanel.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmMomentaryPB.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmSingleButton.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmEngineControl.js")
+// $.getScript(mmDomain + "plugins/midnetmedia/mmTwoAction.js")
+
+// $("<link/>", {
+//     rel: "stylesheet",
+//     type: "text/css",
+//     href: mmDomain + "plugins/midnetmedia/mmPlugins.css"
+//  }).appendTo("head")
+
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/singleMonitor.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/monitorGrid.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmWatchdog.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmValveControl.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmCruiseControl.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmInfoPanel.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmMomentaryPB.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmSingleButton.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmEngineControl.js")
+$.getScript("https://mmfreeboard.netlify.app/plugins/midnetmedia/mmTwoAction.js")
 
 $("<link/>", {
     rel: "stylesheet",
     type: "text/css",
-    href: mmDomain + "plugins/midnetmedia/mmPlugins.css"
+    href: "https://mmfreeboard.netlify.app/plugins/midnetmedia/mmPlugins.css"
  }).appendTo("head")
